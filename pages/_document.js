97ad641d57fe6db990562c450f5e457bf370df53
FakeColdMilk <aaron.nylund2@gmail.com>
2025-11-13 13:07:46 +0200

    yurr

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* External stylesheets must be added here in Document (not in page Head) */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Comfortaa:700"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
